a7edaff43f1862474defb2fe95b71326988a2b94
Gabriel Gassner <Gabriel.gassner@outlook.com>
2023-09-12 22:29:23 -0300

    Criação de novo Controller, e associação de Views e Model;

---
+++ WebApplication1/Models/AlunoModel2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AlunoModel2
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public DateTime Matricula { get; set; }
+        public int Faltas { get; set; }
+
+        public AlunoModel2()
+        {
+            Nome = "Rodrigo";
+            Curso = "Desenvolvimento de Sistemas";
+            Faltas = 1;
+            Matricula = new DateTime(2023,1,27);
+        }
+
+        public static AlunoModel2 CriarEmerson()
+        {
+            var aluno = new AlunoModel2();
+            aluno.Nome = "Emerson";
+            return aluno;
+
+        }
+    }
+}

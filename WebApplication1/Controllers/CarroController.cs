00148937d33df6a18ef774a20497d2b471840d00
Gabriel G <gabriel.gassner@outlook.com>
2023-09-22 20:38:23 -0300

    Import de nova Model;
    Importado de todo o MVC do contexto Carro.

---
+++ WebApplication1/Controllers/CarroController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        public ActionResult Index()
+        {
+            return View(CarroModel.CriarLista());
+        }
+
+        // GET: Carro/Details/5
+        public ActionResult Details(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+        // GET: Carro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carro/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CarroModel collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Carro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Carro/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Carro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Carro/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
